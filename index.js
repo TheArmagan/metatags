aabdd601813f34d88938cef9ecb679db4628e884
Kıraç Armağan Önal <kiracarmaganonal@gmail.com>
2021-02-12 18:41:55 +0300

    yeah

--- index.js
+++ index.js
@@ -4,11 +4,11 @@ const app = express();
 app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
-  res.send("OGPP");
+  return res.render("index.ejs", { color: "0079FC", tags: { title: "Open Graph Protocol Proxy", description: "Example: /0079FC?title=Open+Graph+Protocol+Proxy" } });
 })
 
 app.get("/:color", (req, res) => {
-  return res.render("index.ejs", { color: req.params.color, tags: Object.entries(req.query) })
+  return res.render("index.ejs", { color: req.params.color, tags: Object.entries(req.query) });
 });
 
 app.listen(process.env.PORT || 8080);
