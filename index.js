c121b0a4d98659427e0c7fe188b2eeee35ce8d8f
Kıraç Armağan Önal <kiracarmaganonal@gmail.com>
2021-02-13 20:20:57 +0300

    oh yeah

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
-  return res.render("index.ejs", { color: "0079FC", tags: { title: "MetaTag", description: "Example: /0079FC?og:title=MetaTags!" } });
+  return res.render("index.ejs", { color: "0079FC", tags: [["title", "MetaTag"], ["description", "Example: /0079FC?og:title=MetaTags!"]] });
 })
 
 app.get("/:color", (req, res) => {
